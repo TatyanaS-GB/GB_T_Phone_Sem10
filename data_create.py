f1b5428c4c27676c7cf98d7067461e8455284f3b
Tatyana Stasenko <sale_rim@mail.ru>
2024-06-15 22:09:51 +0700

    commit_1

---
+++ data_create.py
@@ -0,0 +1,20 @@
+def name_data():
+    name = input("Введите имя: ")
+    print("Красивое имя")
+    return name
+
+def  surname_data() -> object:
+    surname = input("Введите фамилию: ")
+    print("Хорошо")
+    return surname
+
+def phone_data():
+        phone = input("Введите телефон: ")
+        print("Хорошо")
+        return phone
+
+
+def address_data():
+    address = input("Введите адресс: ")
+    print("Хорошо")
+    return address
